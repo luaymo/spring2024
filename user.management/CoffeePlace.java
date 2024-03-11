ed20743313a3e5e8ed03897fc33c3cd49bdb8d7f
luaymo <149573345+luaymo@users.noreply.github.com>
2024-03-11 14:48:41 +0500

    Create CoffeePlace.java

---
+++ user.management/CoffeePlace.java
@@ -0,0 +1,9 @@
+package com.coffeeplace.domain;
+
+public class CoffeePlace {
+    private Long id;
+    private String name;
+    private String address;
+    private String description;
+    // геттеры и сеттеры
+}
