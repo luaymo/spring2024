b55134e5f5df2d926668adcfa172a63b84d0481b
luaymo <149573345+luaymo@users.noreply.github.com>
2024-03-11 14:29:01 +0500

    Update User.java

--- user.management/User.java
+++ user.management/User.java
@@ -1,1 +1,10 @@
+package com.example.domain;
+
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String username;
+    // геттеры и сеттеры
+}
 
