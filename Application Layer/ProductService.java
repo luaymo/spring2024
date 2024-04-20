1e67a498d4e745a0c68c1f1864c66fe8950d854b
luaymo <149573345+luaymo@users.noreply.github.com>
2024-04-21 00:04:14 +0500

    ProductService.java

---
+++ Application Layer/ProductService.java
@@ -0,0 +1,10 @@
+// ProductService class
+import java.util.List;
+
+public class ProductService {
+    public List<Product> getAllProducts() {
+        // Logic to fetch all products from database
+        return null;
+    }
+    // Other product related methods
+}
