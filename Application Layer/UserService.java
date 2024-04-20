a2ab00a4c5f2276545ca7fc82f2898f8a7a33b82
luaymo <149573345+luaymo@users.noreply.github.com>
2024-04-21 00:03:39 +0500

    UserService.java

---
+++ Application Layer/UserService.java
@@ -0,0 +1,8 @@
+// UserService class
+public class UserService {
+    public User getUserById(String userId) {
+        // Logic to fetch user from database
+        return null;
+    }
+    // Other user related methods
+}
