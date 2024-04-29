fd752d23482fc1a1139cea3fdb22590827d4b340
luaymo <149573345+luaymo@users.noreply.github.com>
2024-04-29 15:59:11 +0500

    Create OrderService.java

---
+++ Service Layer/OrderService.java
@@ -0,0 +1,10 @@
+package com.coffeeshop.service;
+
+import com.coffeeshop.domain.Order;
+
+public class OrderService {
+    public void placeOrder(Order order) {
+        // Logic to place an order
+    }
+    // Other order related methods
+}
