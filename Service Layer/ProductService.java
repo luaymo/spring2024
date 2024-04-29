2ba93c2e8f51cdd4f45277c0c9d74254459821c4
luaymo <149573345+luaymo@users.noreply.github.com>
2024-04-29 15:58:28 +0500

    Create ProductService.java

---
+++ Service Layer/ProductService.java
@@ -0,0 +1,13 @@
+package com.coffeeshop.service;
+
+import com.coffeeshop.domain.Product;
+
+import java.util.List;
+
+public class ProductService {
+    public List<Product> getAllProducts() {
+        // Logic to fetch all products from database
+        return null;
+    }
+    // Other product related methods
+}
