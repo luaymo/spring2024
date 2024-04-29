bb5ca256c1cbce89680b72d358704852ef572f68
luaymo <149573345+luaymo@users.noreply.github.com>
2024-04-29 15:57:58 +0500

    Create UserService.java

---
+++ Service Layer/UserService.java
@@ -0,0 +1,11 @@
+package com.coffeeshop.service;
+
+import com.coffeeshop.domain.User;
+
+public class UserService {
+    public User getUserById(String userId) {
+        // Logic to fetch user from database
+        return null;
+    }
+    // Other user related methods
+}
