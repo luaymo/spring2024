e9a696d98a4004e4f648fcccf2961609d15a2a0a
luaymo <149573345+luaymo@users.noreply.github.com>
2024-05-13 18:22:44 +0500

    Create ReviewDTO.java

---
+++ Infrastructure Layer (infrastructure package)/Model Layer/ReviewDTO.java
@@ -0,0 +1,10 @@
+package com.coffeeplace.coffeeshopapp.infrastructure.model;
+
+public class ReviewDTO {
+    private Long id;
+    private Long userId;
+    private String comment;
+    private int rating;
+    
+    // constructors, getters, and setters
+}
