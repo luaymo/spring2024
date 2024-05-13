a78792fc9aadefb0a3676b1a3c515bb67116d279
luaymo <149573345+luaymo@users.noreply.github.com>
2024-05-13 18:22:25 +0500

    Create UserDTO.java

---
+++ Infrastructure Layer (infrastructure package)/Model Layer/UserDTO.java
@@ -0,0 +1,9 @@
+package com.coffeeplace.coffeeshopapp.infrastructure.model;
+
+public class UserDTO {
+    private Long id;
+    private String email;
+    private String username;
+    
+    // constructors, getters, and setters
+}
