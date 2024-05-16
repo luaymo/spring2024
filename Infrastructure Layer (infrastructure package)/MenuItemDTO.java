a9ad609c04ebc76c9dc3b954f87e18a2e4114cf1
luaymo <149573345+luaymo@users.noreply.github.com>
2024-05-16 19:09:49 +0500

    MenuItemDTO.java (Задания 13)

---
+++ Infrastructure Layer (infrastructure package)/MenuItemDTO.java
@@ -0,0 +1,10 @@
+package com.coffeeplace.coffeeshopapp.infrastructure.model;
+
+public class MenuItemDTO {
+    private Long id;
+    private String name;
+    private double price;
+    private String description;
+    
+    // constructors, getters, and setters
+}
