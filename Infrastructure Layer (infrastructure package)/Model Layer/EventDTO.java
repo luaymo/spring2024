a8c99cdf5a57816779ea4e1cbfb84019e0e51e64
luaymo <149573345+luaymo@users.noreply.github.com>
2024-05-26 16:25:30 +0500

    EventDTO.java (Задания 14)

---
+++ Infrastructure Layer (infrastructure package)/Model Layer/EventDTO.java
@@ -0,0 +1,12 @@
+package com.coffeeplace.coffeeshopapp.infrastructure.model;
+
+import java.time.LocalDate;
+
+public class EventDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate date;
+
+    // constructors, getters, and setters
+}
