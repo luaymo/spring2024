4879ebfc2db07dcb8f63515002fb40bd17516f84
luaymo <149573345+luaymo@users.noreply.github.com>
2024-05-26 16:28:30 +0500

    LocationDTO.java (Задания 14)

---
+++ Infrastructure Layer (infrastructure package)/Model Layer/LocationDTO.java
@@ -0,0 +1,10 @@
+package com.coffeeplace.coffeeshopapp.infrastructure.model;
+
+public class LocationDTO {
+    private Long id;
+    private String name;
+    private double latitude;
+    private double longitude;
+
+    // constructors, getters, and setters
+}
