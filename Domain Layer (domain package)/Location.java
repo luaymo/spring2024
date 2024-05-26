8b5eb7a6ff21da8a7fc0a1dadde33b974b52ad55
luaymo <149573345+luaymo@users.noreply.github.com>
2024-05-26 16:13:42 +0500

    Location.java (Задания 14)

---
+++ Domain Layer (domain package)/Location.java
@@ -0,0 +1,18 @@
+package com.coffeeplace.coffeeshopapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double latitude;
+    private double longitude;
+    
+    // constructors, getters, and setters
+}
