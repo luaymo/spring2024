d9957ba5cacd148d32f150c70578006262977efb
luaymo <149573345+luaymo@users.noreply.github.com>
2024-05-26 16:11:43 +0500

    Promotion.java (Задания 14)

---
+++ Domain Layer (domain package)/Promotion.java
@@ -0,0 +1,20 @@
+package com.coffeeplace.coffeeshopapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Promotion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    
+    // constructors, getters, and setters
+}
