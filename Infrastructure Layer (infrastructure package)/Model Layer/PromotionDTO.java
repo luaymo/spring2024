6d0207d3aef59f00be2148636749fe54e9a6ed6a
luaymo <149573345+luaymo@users.noreply.github.com>
2024-05-26 16:26:35 +0500

    PromotionDTO.java (Задания 14)

---
+++ Infrastructure Layer (infrastructure package)/Model Layer/PromotionDTO.java
@@ -0,0 +1,13 @@
+package com.coffeeplace.coffeeshopapp.infrastructure.model;
+
+import java.time.LocalDate;
+
+public class PromotionDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    // constructors, getters, and setters
+}
