a81bded0c01ab4a6b63256d30795acc766f0eb88
luaymo <149573345+luaymo@users.noreply.github.com>
2024-05-26 16:12:36 +0500

    LoyaltyProgram.java (Задания 14)

---
+++ Domain Layer (domain package)/LoyaltyProgram.java
@@ -0,0 +1,18 @@
+package com.coffeeplace.coffeeshopapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class LoyaltyProgram {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private int points;
+    
+    // constructors, getters, and setters
+}
