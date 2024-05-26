fb8f7612aea863204df1b83ee7d7c80ccb128228
luaymo <149573345+luaymo@users.noreply.github.com>
2024-05-26 16:27:49 +0500

    LoyaltyProgramDTO.java (Задания 14)

---
+++ Infrastructure Layer (infrastructure package)/Model Layer/LoyaltyProgramDTO.java
@@ -0,0 +1,10 @@
+package com.coffeeplace.coffeeshopapp.infrastructure.model;
+
+public class LoyaltyProgramDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private int points;
+
+    // constructors, getters, and setters
+}
