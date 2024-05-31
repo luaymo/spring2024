8886c829beb446f7982abd2413267523c5dc2a05
luaymo <149573345+luaymo@users.noreply.github.com>
2024-05-31 16:26:16 +0500

    EducationalContentDTO.java (Задания 15)

---
+++ Infrastructure Layer (infrastructure package)/Model Layer/EducationalContentDTO.java
@@ -0,0 +1,9 @@
+package com.coffeeplace.coffeeshopapp.infrastructure.model;
+
+public class EducationalContentDTO {
+    private Long id;
+    private String title;
+    private String content;
+
+    // constructors, getters, and setters
+}
