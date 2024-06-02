863e160eb93f3128bd835aa56685b6e1f300efd9
luaymo <149573345+luaymo@users.noreply.github.com>
2024-06-02 18:02:05 +0500

    FavoriteCoffeeShopDTO.java (Задания 15)

---
+++ Model Layer (model package)/FavoriteCoffeeShopDTO.java
@@ -0,0 +1,9 @@
+package com.coffeeplace.coffeeshopapp.infrastructure.model;
+
+public class FavoriteCoffeeShopDTO {
+    private Long id;
+    private Long userId;
+    private Long coffeeShopId;
+
+    // constructors, getters, and setters
+}
