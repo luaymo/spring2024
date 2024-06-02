1da29999d1e6c84b5222375e9442cb0bebd9e031
luaymo <149573345+luaymo@users.noreply.github.com>
2024-06-02 18:01:29 +0500

    CoffeeBeanDTO.java (Задания 15)

---
+++ Model Layer (model package)/CoffeeBeanDTO.java
@@ -0,0 +1,10 @@
+package com.coffeeplace.coffeeshopapp.infrastructure.model;
+
+public class CoffeeBeanDTO {
+    private Long id;
+    private String name;
+    private String origin;
+    private String roastLevel;
+
+    // constructors, getters, and setters
+}
